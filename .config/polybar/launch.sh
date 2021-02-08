61972b1f58a12b0a4994765ad602e6e21953f9ab
Panagiotis A Natsis <pnotz17@gmail.com>
2021-02-08 18:28:00 +0200

    changes in dotfiles

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,14 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  # Launch mybar for each monitor
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
-  done
-else
-  # Launch mybar
-  polybar --reload mybar &
-fi
+# Launch bar1 and bar2
+polybar --reload mybar -c ~/.config/polybar/config &
 
 echo "Bars launched..."
