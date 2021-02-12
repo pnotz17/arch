1497d17f136cc5a3ad2958eb6621371ea613b270
Panagiotis A Natsis <pnotz17@gmail.com>
2021-02-12 10:14:13 +0200

    changes in dotfiles

--- .local/bin/updates.sh
+++ .local/bin/updates.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-updates=$(checkupdates | wc -l)
+if [ -s /var/local/countupdates/count ] ; then
+    updates=$(cat /var/local/countupdates/count)
+else
+    updates=0
+fi
 
 if [ "$updates" -gt 0 ]; then
-    echo " System Updates:"
+    echo " System Updates: $updates"
 else
     echo ""
 fi
