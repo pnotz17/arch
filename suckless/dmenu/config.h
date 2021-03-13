f4bf03a03c4d797b040ce04294d6a3c8eccb5a5e
Panagiotis A Natsis <pnotz17@gmail.com>
2021-03-13 22:54:24 +0200

    changes in dotfiles

--- suckless/dmenu/config.h
+++ suckless/dmenu/config.h
@@ -5,8 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"UbuntuMono Nerd Font:size=12"
-};
+static const char *fonts[]          = { "UbuntuMono Nerd Font:pixelsize=21:antialias=true:autohint=true", "Noto Color Emoji:style=Regular:size=10:antialias=true:autohint=true" };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
