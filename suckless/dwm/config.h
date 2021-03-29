125a3729823747a19c7e2bcfea49aae1ebd109a1
Panagiotis A Natsis <pnotz17@gmail.com>
2021-03-29 08:29:49 +0300

    changes in dotfiles

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,5 +1,5 @@
-static const char *fonts[]	= {"TerminessTTF Nerd Font:style=Medium:size=11:antialias=true:autohint=true","Noto Color Emoji:style=Regular:size=10:antialias=true:autohint=true","Font Awesome:size:10:antialias=true:autohint=true",};
-static const char dmenufont[] =  {"TerminessTTF Nerd Font:style=Medium:size=11:antialias=true:autohint=true"};
+static const char *fonts[]	= {"DaddyTimeMono Nerd Font:style=Book:size=9:antialias=true:autohint=true","EmojiOne:style=Regular:size=9:antialias=true:autohint=true","Font Awesome:size:10:antialias=true:autohint=true",};
+static const char dmenufont[] =  {"DaddyTimeMono Nerd Font:style=Book:size=9:antialias=true:autohint=true"};
 static const unsigned int borderpx = 1;/* border pixel of windows */
 static const unsigned int gappx = 10;/* gaps between windows */
 static const unsigned int snap = 32;/* snap pixel */
