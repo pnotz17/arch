d668ffbf285f5b7eda6830df807a382f285f7db0
Panagiotis A Natsis <pnotz17@gmail.com>
2021-03-31 00:36:13 +0300

    changes in dotfiles

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -106,14 +106,14 @@ static uint forcemousemod = ShiftMask;
 
  // Internal mouse shortcuts.
 static MouseShortcut mshortcuts[] = {
-	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,				Button4, kscrollup,				{.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,				Button5, kscrolldown,		{.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,				Button2, selpaste,				{.i = 0},      1 },
-	{ ShiftMask,						Button4, ttysend,				{.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,				Button4, ttysend,				{.s = "\031"} },
-	{ ShiftMask,						Button5, ttysend,				{.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,				Button5, ttysend,				{.s = "\005"} },
+	/* mask                 button   function        	argument       release */
+	{ XK_ANY_MOD,		Button4, kscrollup,		{.i = 1},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,		Button5, kscrolldown,		{.i = 1},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,		Button2, selpaste,		{.i = 0},      1 },
+	{ ShiftMask,		Button4, ttysend,		{.s = "\033[5;2~"} },
+	{ XK_ANY_MOD,		Button4, ttysend,		{.s = "\031"} },
+	{ ShiftMask,		Button5, ttysend,		{.s = "\033[6;2~"} },
+	{ XK_ANY_MOD,		Button5, ttysend,		{.s = "\005"} },
 };
 
 /* Internal keyboard shortcuts. */
@@ -121,24 +121,24 @@ static MouseShortcut mshortcuts[] = {
 #define TERMMOD (Mod4Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
-	/* mask                 			   keysym          			   function                 argument */
-	{ TERMMOD,						XK_Prior,						zoom,						{.f = +1} },
-	{ TERMMOD, 						XK_Next,						zoom,						{.f = -1} },
-	{ MODKEY, 							XK_Home,        			zoomreset,				{.f =  0} },
-	{ MODKEY,							XK_c,           					clipcopy,					{.i =  0} },
-	{ ShiftMask,						XK_Insert,					clippaste,				{.i =  0} },
-	{ MODKEY,							XK_v,								clippaste,				{.i =  0} },
-	{ XK_ANY_MOD,				Button2,						selpaste,					{.i =  0} },
-	{ ShiftMask,            			XK_Page_Up,     		kscrollup,  		    	{.i = -1} },
-	{ ShiftMask,            			XK_Page_Down,   	kscrolldown,    		{.i = -1} },
-	{ MODKEY,               			XK_Page_Up,     		kscrollup,      			{.i = -1} },
-	{ MODKEY,               			XK_Page_Down,   	kscrolldown,    		{.i = -1} },
-    { MODKEY,               			XK_Up,          				kscrollup,      			{.i =  1} },
-	{ MODKEY,               			XK_Down,        			kscrolldown,    		{.i =  1} },
-    { MODKEY,               			XK_u,           					kscrollup,      			{.i = -1} },
-	{ MODKEY,               			XK_d,           					kscrolldown,    		{.i = -1} },
-	{ TERMMOD,              		XK_Up,							zoom,           			{.f = +1} },
-	{ TERMMOD,              		XK_Down,        			zoom,           			{.f = -1} },
+	/* mask         keysym          function         argument */
+	{ TERMMOD,	XK_Prior,	zoom,		{.f = +1} },
+	{ TERMMOD, 	XK_Next,	zoom,		{.f = -1} },
+	{ MODKEY, 	XK_Home,        zoomreset,	{.f =  0} },
+	{ MODKEY,	XK_c,           clipcopy,	{.i =  0} },
+	{ ShiftMask,	XK_Insert,	clippaste,	{.i =  0} },
+	{ MODKEY,	XK_v,		clippaste,	{.i =  0} },
+	{ XK_ANY_MOD,	Button2,	selpaste,	{.i =  0} },
+	{ ShiftMask,    XK_Page_Up,     kscrollup,  	{.i = -1} },
+	{ ShiftMask,    XK_Page_Down,  	kscrolldown,    {.i = -1} },
+	{ MODKEY,       XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ MODKEY,       XK_Page_Down,  	kscrolldown,    {.i = -1} },
+    	{ MODKEY,       XK_Up,          kscrollup,      {.i =  1} },
+	{ MODKEY,       XK_Down,        kscrolldown,    {.i =  1} },
+    	{ MODKEY,       XK_u,          	kscrollup,      {.i = -1} },
+	{ MODKEY,       XK_d,          	kscrolldown,    {.i = -1} },
+	{ TERMMOD,      XK_Up,		zoom,           {.f = +1} },
+	{ TERMMOD,      XK_Down,        zoom,           {.f = -1} },
 };
 /*
  * Special keys (change & recompile st.info accordingly)
