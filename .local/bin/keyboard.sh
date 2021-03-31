5c78fe7157e0e5f8b672354f1f35e7278f46136b
Panagiotis A Natsis <pnotz17@gmail.com>
2021-03-31 20:02:06 +0300

    changes in dotfiles

---
+++ .local/bin/keyboard.sh
@@ -0,0 +1,13 @@
+   #!/usr/bin/env bash
+   #
+   # ~/bin/keyboard—In case of new keyboard, break gla...er...I mean...run this.
+   #
+
+   (
+     sleep 1
+
+       DISPLAY=:0.0 xset -b
+         DISPLAY=:0.0 xset r rate 330 60
+	   xcape -e 'Control_L=Escape'
+	     DISPLAY=:0.0 notify-send "External USB Connected"
+     ) &
