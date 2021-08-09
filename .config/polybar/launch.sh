84af71ecb6414eedb502164089c0ca207aad65ee
Panagiotis A Natsis <pnotz17@gmailcom>
2021-08-09 10:11:36 +0000

    changes in dotfiles

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar --reload mybar -c ~/.config/polybar/config &
+polybar --reload bspwm -c ~/.config/polybar/config &
 
 echo "Bars launched..."
