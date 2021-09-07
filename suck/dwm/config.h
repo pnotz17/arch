a9c40215d4c55b9a7b2376a30cf832df4258f03e
Panagiotis A Natsis <pnotz17@gmailcom>
2021-09-07 23:31:18 +0300

    changes in dotfiles

--- suck/dwm/config.h
+++ suck/dwm/config.h
@@ -1,5 +1,5 @@
 static const char *fonts[]            ={"Monospace:style=Regular:size=10:antialias=true:autohint=true","EmojiOne:style=Regular:size=10:antialias=true:autohint=true",};
-static const char normbgcolor[]       = "#111111";	/* bar backround color */
+static const char normbgcolor[]       = "#121213";	/* bar backround color */
 static const char normfgcolor[]       = "#FFFFFF";	/* bar foreground color on right & left*/
 static const char selbgcolor[] 	      = "#000000";	/* highlighted tag // tasklist // focused window background color*/
 static const char selfgcolor[] 	      = "#FFFFFF";	/* focused tag and tasklist foreground color*/
