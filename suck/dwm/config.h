a284c724ab5d6f90796594344e31de8dead7ac3c
Panagiotis A Natsis <pnotz17@gmailcom>
2021-09-07 23:25:10 +0300

    changes in dotfiles

--- suck/dwm/config.h
+++ suck/dwm/config.h
@@ -1,13 +1,13 @@
 static const char *fonts[]            ={"Monospace:style=Regular:size=10:antialias=true:autohint=true","EmojiOne:style=Regular:size=10:antialias=true:autohint=true",};
-static const char normbgcolor[]       = "#000000";	/* bar backround color */
+static const char normbgcolor[]       = "#111111";	/* bar backround color */
 static const char normfgcolor[]       = "#FFFFFF";	/* bar foreground color on right & left*/
 static const char selbgcolor[] 	      = "#000000";	/* highlighted tag // tasklist // focused window background color*/
 static const char selfgcolor[] 	      = "#FFFFFF";	/* focused tag and tasklist foreground color*/
-static const char unselbordercolor[]  = "#B3AFC2";	/* unfocused window border color*/
-static const char selbordercolor[]    = "#FF0000";	/* focused window border color*/
+static const char unselbordercolor[]  = "#000000";	/* unfocused window border color*/
+static const char selbordercolor[]    = "#B3AFC2";	/* focused window border color*/
 static const unsigned int borderpx    = 1;              /* border pixel of windows */
 static const unsigned int gappx       = 31;              /* gaps between windows */
-static const unsigned int snap        = 32;             /* snap pixel */
+static const unsigned int snap        = 33;             /* snap pixel */
 static const int showbar              = 1;              /* 0 means no bar */
 static const int topbar               = 1;              /* 0 means bottom bar */
 static const unsigned int baralpha    = 0xd0;
